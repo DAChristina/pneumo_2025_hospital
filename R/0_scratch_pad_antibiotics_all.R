d26dd1d3426b61bae7a69630e7ea43d046ea9d54
DAChristina <chrisdewiress@gmail.com>
2025-09-05 10:59:49 +0700

    data cleaning & viz

---
+++ R/0_scratch_pad_antibiotics_all.R
@@ -0,0 +1,170 @@
+library(tidyverse)
+source("global/fun.R")
+
+# cleanup other antibiotics data
+# list of all other antibiotics
+name_abx <- c(
+  "amikasin" = "amikacin",
+  "ainophylin" = "aminophylline",
+  "aminophylin" = "aminophylline",
+  "aninophylin" = "aminophylline",
+
+  "cefazoline" = "cefazolin",
+  "cefixim" = "cefixime",
+  "cefixime oral" = "cefixime",
+  "cefobactam" = "cefoperazone_sulbactam", # cefoperazone + sulbactam
+  "cefoperazone" = "cefoperazone_sulbactam", # different but no individual cefoperazone detected
+  "cefoperazol sulbactam" = "cefoperazone_sulbactam",
+  "cefoperazol" = "cefoperazone_sulbactam",
+  "cefotaxim" = "cefotaxime",
+  "ceftadizime" = "ceftazidime",
+  "kotrimoxazol" = "cotrimoxazole",
+  
+  "genta" = "gentamicin",
+  "gentamicyn" = "gentamicin",
+  "gentamisin" = "gentamicin",
+  "gentamycin" = "gentamicin",
+  "gentanycin" = "gentamicin",
+  "levoflixacin" = "levofloxacin",
+  "meropenem injeksi" = "meropenem",
+  "metrinidazole" = "metronidazole",
+  "metro" = "metronidazole",
+  
+  "oat (rifampisin, inh, pirazinamid, etambutol)" = "OAT (Rifampicin, Isoniazid, Pyrazinamide, Ethambutol)",
+  "rifampisin" = "rifampicin",
+  "inh" = "isoniazid",
+  "pirazinamid" = "pyrazinamide",
+  "etambutol" = "ethambutol",
+  
+  "vancomicin" = "vancomycin",
+  "vicillin" = "vicilin"
+)
+
+weird_abx <- c("oral", "injeksi", "dan",
+               "oat",
+               "sulbactam", "sulbaktam"
+               )
+
+cleaned_other_abx <- df_epi_hospital %>%
+  dplyr::rename(other_antibiotics = jika_lainnya_sebutkan_nama_antibiotik_nya) %>%
+  dplyr::rowwise() %>% # crucial!
+  dplyr::mutate(other_antibiotics_cleaned = clean_values(text = other_antibiotics,
+                                                         dict = name_abx,
+                                                         exclude = weird_abx)
+                ) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(
+    no_id_subjek,
+    other_antibiotics,
+    other_antibiotics_cleaned
+  ) %>%
+  # view() %>%
+  glimpse()
+
+test <- cleaned_other_abx %>% 
+  dplyr::filter(stringr::str_detect(other_antibiotics_cleaned, "cefopera")) %>% 
+  # view() %>%
+  glimpse()
+
+all_unique_abx <- cleaned_other_abx %>%
+  dplyr::pull(other_antibiotics_cleaned) %>%
+  stringr::str_split(", ") %>%
+  unlist() %>%
+  unique() %>%
+  sort() %>% 
+  # view() %>% 
+  glimpse()
+
+df_one_hot_encoded <- cleaned_other_abx %>%
+  dplyr::select(-other_antibiotics) %>% 
+  dplyr::filter(!is.na(other_antibiotics_cleaned)) %>%
+  tidyr::separate_rows(other_antibiotics_cleaned, sep = ", ") %>%
+  dplyr::mutate(value = as.numeric(1)) %>%
+  tidyr::pivot_wider(
+    id_cols = no_id_subjek,
+    names_from = other_antibiotics_cleaned,
+    values_from = value,
+    values_fill = 0,
+    values_fn = length
+  ) %>%
+  dplyr::mutate(
+    across(where(is.list), ~ as.numeric(.)),
+    # across(where(is.list), ~ ifelse(map_lgl(., is.null), 0, .)),
+    across(where(is.numeric), ~ replace_na(., 0))
+  ) %>%
+  # view() %>% 
+  glimpse()
+
+dplyr::left_join(
+  df_epi_hospital %>% 
+    dplyr::select(
+      no_id_subjek,
+      contains("jika_ya_manakah_antibiotik_di_bawah_ini_yang_diberikan_")
+    ) %>% 
+    dplyr::rename_with(~ str_remove(., "jika_ya_manakah_antibiotik_di_bawah_ini_yang_diberikan_")
+    )
+  ,
+  df_one_hot_encoded
+  ,
+  by = "no_id_subjek"
+) %>% 
+  dplyr::select(-no_id_subjek,
+                -lainnya) %>% 
+  dplyr::mutate(
+    across(everything(), ~ as.numeric(.)),
+    across(where(is.numeric), ~ replace_na(., 0))
+  ) %>%
+  dplyr::rename(
+    # have already in 1 and 0
+    "amoxicillin" = amoksisilin,
+    "amoxicillin-clavulanic acid" = amoksisilin_as_klavulanat,
+    "ampicillin" = ampisilin,
+    "ampicillin-sulbactam" = ampisilin_sulbaktam,
+    "azithromycin" = azitromisin,
+    "cefotaxime main" = sefotaksim,
+    "ceftriaxone" = seftriakson,
+    "erythromycin" = eritromisin,
+    "trimethoprim-sulfamethoxazole" = trimetoprim_sulfametoksazol
+  ) %>% 
+  # need to be discussed: NA in antibiotics usage
+  dplyr::filter(!is.na(amoxicillin)
+  ) %>% 
+  UpSetR::upset(
+    sets = c("amoxicillin",
+             "amoxicillin-clavulanic acid",
+             "ampicillin",
+             "ampicillin-sulbactam",
+             "azithromycin",
+             "cefotaxime main",
+             "ceftriaxone",
+             "erythromycin",
+             "trimethoprim-sulfamethoxazole",
+             
+             "meropenem",
+             "vancomycin",
+             "amikacin",
+             "ceftazidime",
+             "gentamicin",
+             "metronidazole",
+             "cefixime",
+             "cefoperazone_sulbactam",
+             "cefazolin",
+             "ethambutol",
+             "isoniazid",
+             "pyrazinamide",
+             "rifampicin",
+             "levofloxacin",
+             "ciprofloxacin",
+             "cotrimoxazole",
+             "aminophylline",
+             "vicilin",
+             "cefotaxime"
+             
+    ),
+    order.by = "freq",
+    empty.intersections = "on",
+    number.angles = 0,
+    point.size = 3.5,
+    line.size = 2, 
+    mainbar.y.label = "Antibiotics Combinations",
+    sets.x.label = "Patients with Antibiotics")
---
+++ R/0_scratch_pad_tanggal.R
@@ -0,0 +1,119 @@
+df <- df_epi_hospital %>% 
+  filter(
+    # lama_di_rs_hari_dc < 0 | lama_di_rs_hari_dc >= 50 # some patients spent >= 50 days in RS
+  ) %>% 
+  select(contains(c("tanggal_")),
+         start, end, no_id_subjek,
+         -contains(c("pcv", "tanggal_pengambilan")),
+         -tanggal_pengisian_form,
+         -tanggal_pengisian_crf,
+         -tanggal_pemeriksaan_rontgen_thorax,
+         -tanggal_tanda_tangan_persetujuan_informed_consent
+         ) %>% 
+  glimpse()
+
+hist(df_epi_hospital$lama_di_rs_hari_dc, breaks = 100)
+
+# LOS calculations overall/per-ageGroups/per-area
+# must clean up prematurity & complications first!
+summary_LOS <- df_epi_hospital %>%
+  rename(prematur = diantara_daftar_nama_penyakit_di_bawah_ini_manakah_penyakit_medis_atau_kondisi_kronik_yang_mendasari_pasien_prematuritas) %>% 
+  mutate(prematur = ifelse(prematur == 1, "Ya", "Tidak")) %>% 
+  group_by(grup_usia_vaksin, prematur) %>% 
+  summarise(
+    median_los = median(lama_di_rs_hari_dc, na.rm = TRUE),
+    iqr_los = IQR(lama_di_rs_hari_dc, na.rm = TRUE),
+    q1 = quantile(lama_di_rs_hari_dc, 0.25, na.rm = TRUE),
+    q3 = quantile(lama_di_rs_hari_dc, 0.75, na.rm = TRUE)
+  ) %>% 
+  view() %>% 
+  glimpse()
+
+summary_LOS %>% 
+  ggplot(., aes(x = prematur)) +
+  geom_point(aes(y = iqr_los), colour = "darkgreen", size = 2) +
+  geom_errorbar(aes(
+    ymin = q1,
+    ymax = q3,
+    width = .1),
+    colour = "darkgreen") +
+  geom_point(aes(y = median_los), colour = "steelblue", size = 2) +
+  theme_bw() +
+  facet_wrap(~grup_usia_vaksin)
+
+
+df_long <- df %>%
+  pivot_longer(cols = starts_with("tanggal_"), names_to = "tanggal_type", values_to = "tanggal_")
+
+no_id_subjek_order <- df_long %>%
+  group_by(no_id_subjek) %>%
+  summarise(min_tanggal_ = min(tanggal_, na.rm = TRUE)) %>%
+  arrange(min_tanggal_) %>% # earlier date
+  pull(no_id_subjek)
+
+df_long <- df_long %>%
+  mutate(no_id_subjek = factor(no_id_subjek, levels = no_id_subjek_order)) %>% 
+  glimpse()
+
+df_segments <- df_long %>%
+  group_by(no_id_subjek) %>%
+  arrange(tanggal_) %>%
+  mutate(next_date = lead(tanggal_)) %>%
+  filter(!is.na(next_date))  # Remove last point
+
+ggplot(df_long, aes(x = as.Date(tanggal_), y = no_id_subjek, color = tanggal_type)) +
+  geom_point(size = 3) +
+  geom_segment(
+    data = df_segments,
+    aes(x = as.Date(tanggal_), xend = as.Date(next_date),
+        y = no_id_subjek, yend = no_id_subjek),
+    arrow = arrow(length = unit(0.2, "cm")),
+    color = "steelblue",
+    size = 0.2,
+    inherit.aes = FALSE
+  ) +
+  scale_color_brewer(palette = "Set1") +
+  theme_minimal() +
+  scale_x_date(date_labels = "%Y", date_breaks = "1 year",
+               # limits = c(as.Date("2023-06-01"), as.Date(max(df_long$tanggal_)))
+               ) +
+  labs(x = "Date", y = "ID", color = "Date Type")
+  # theme(#axis.text.y = element_text(size = 10),
+  #       #axis.text.x = element_text(angle = 45, hjust = 1, size = 10),
+  #       # legend.position = c(0.02, 0.75),
+  #       # legend.direction = "horizontal",
+  #       # legend.justification = c("left", "top"),
+  #       legend.background = element_rect(fill = NA, color = NA),
+  #       legend.title = element_blank(),
+  #       legend.margin = margin(t = -50),
+  #       legend.spacing.y = unit(-0.3, "cm"))
+
+
+
+# additional arrow:
+df_arrow <- df %>%
+  select(no_id_subjek, tanggal_masuk_rs, tanggal_keluar_rs) %>%
+  filter(!is.na(tanggal_masuk_rs), !is.na(tanggal_keluar_rs)) %>%
+  mutate(
+    tanggal_masuk_rs = as.Date(tanggal_masuk_rs),
+    tanggal_keluar_rs = as.Date(tanggal_keluar_rs)
+  )
+
+# Sort by earliest masuk
+df_arrow <- df_arrow %>%
+  mutate(no_id_subjek = factor(no_id_subjek, levels = df_arrow %>%
+                                 group_by(no_id_subjek) %>%
+                                 summarise(min_date = min(tanggal_masuk_rs)) %>%
+                                 arrange(min_date) %>%
+                                 pull(no_id_subjek)))
+
+
+
+ggplot(df_arrow, aes(x = tanggal_masuk_rs, xend = tanggal_keluar_rs, 
+                     y = no_id_subjek, yend = no_id_subjek)) +
+  geom_segment(arrow = arrow(length = unit(0.2, "cm")), color = "tomato", size = 1) +
+  geom_point(aes(x = tanggal_masuk_rs), color = "darkgreen", size = 2) +
+  geom_point(aes(x = tanggal_keluar_rs), color = "steelblue", size = 2) +
+  theme_minimal() +
+  labs(x = "Date", y = "ID", title = "Hospital Stay (Masuk-Keluar, filter >= 50 hari)") +
+  theme(axis.text.y = element_text(size = 7))
